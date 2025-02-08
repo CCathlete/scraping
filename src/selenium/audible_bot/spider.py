e712a9d336aae0e70db3f1076a100eaa7c41994b
ccat <ccathlete01@gmail.com>
2025-02-08 19:31:52 +0100

    Started Selenium project #1 - audible bot.

---
+++ src/selenium/audible_bot/spider.py
@@ -0,0 +1,13 @@
+"""
+Building the spider.
+"""
+
+from src.imports.selenium_imports import (
+    webdriver,
+    By,
+    WebElement,
+    EC,
+    WebDriverWait,
+)
+import pandas as pd
+from typing import Union
